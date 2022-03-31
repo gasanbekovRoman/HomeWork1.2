a39fbf527215d46fa7dcb60a480314360e113447
GasanbekovRoman <spamerl256@gmail.com>
2022-03-31 17:14:40 +0400

    начало

--- HomeWork1.2/ViewController.swift
+++ HomeWork1.2/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
-    override func viewDidLoad() {
+    @IBOutlet weak var startButton: UIButton!
+    
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    
+     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+         startButton.layer.cornerRadius = 10
+        
+        redView.layer.cornerRadius = 75
+        yellowView.layer.cornerRadius = 75
+        greenView.layer.cornerRadius = 75
+        
+        redView.alpha = 0.3
+        yellowView.alpha = 0.3
+        greenView.alpha = 0.3
+         
+        
+    
     }
-
-
+    
+    @IBAction func showColorButton(_ sender: Any) {
+        
+        if startButton.title == "START"{
+            
+        }
+            
+        
+    }
 }
 
